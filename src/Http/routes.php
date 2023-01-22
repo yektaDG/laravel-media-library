3a1f1782ee3a617d8bd860dc6ee2aff2e206a897
zmilad97 <zmilad97@gmail.com>
2023-01-22 16:26:43 +0330

    removed unnecessary route file

--- src/Http/routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('test', function () {
-    return "shit happens";
-});
--- src/routes/web.php
+++ src/routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('test', function () {
-    return "shit happens";
+    return "test";
 });
