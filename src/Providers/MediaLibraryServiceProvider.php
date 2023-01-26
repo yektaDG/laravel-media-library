24bdbaadd17f725db59adb8015ddcbbec10e255e
zmilad97 <zmilad97@gmail.com>
2023-01-26 17:15:05 +0330

    fixed duplicate route problem

--- src/Providers/MediaLibraryServiceProvider.php
+++ src/Providers/MediaLibraryServiceProvider.php
@@ -15,7 +15,6 @@ class MediaLibraryServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'mediaLibrary');
         $this->publishes([
             __DIR__ . '/../resources/js' => public_path('vendor/yektadg/medialibrary'),
--- src/routes/web.php
+++ src/routes/web.php
@@ -21,22 +21,3 @@ Route::prefix('/media-library')->name('medialibrary.')->controller(MediaControll
         Route::get('/{folder}/get-media', [MediaController::class, 'getMediaByFolder'])->name('folder-media');
     });
 });
-/*Route::resource('/media', MediaController::class)->except('show');
-Route::get('/library/all', [MediaController::class, 'getAllImagesUploadedByCurrentUser'])->name('library.all');
-Route::prefix('/media')->name('media.')->controller(MediaController::class)->group(function () {
-    Route::get('/all', 'getAllImagesUploadedByCurrentUser')->name('all');
-    Route::get('/get-by-folder', 'getImageByGalleryFolder')->name('get-by-folder');
-    Route::post('/remove', 'destroy')->name('remove');
-    Route::post('/get', 'getSingleImage')->name('single');
-    Route::post('/set-alt', 'setAlt')->name('setAlt');
-});
-
-
-Route::post('/util/folder/add-media', [MediaController::class, 'addToFolder'])->name('media.to.folder');
-Route::post('/util/folder/remove', [MediaController::class, 'removeFolder'])->name('media.folder.destroy');
-Route::post('/util/folder/remove-media', [MediaController::class, 'removeMediaFromFolder'])->name('media.folder.remove-media');
-
-Route::get('/util/folder/all', [MediaController::class, 'getAllFolders'])->name('media.folder.all');
-Route::get('/util/folder/{folder}/get-media', [MediaController::class, 'getMediaByFolder'])->name('media.folder.get-media');
-
-Route::post('/image-exists', [MediaController::class, 'imageExists'])->name('image-exists');*/
